541fc05efd3cfdaf4e90835e699c470827a91e8c
shukawam <shukawam@gmail.com>
2022-10-03 19:55:49 +0900

    WIP

--- invoke-other-functions/client/src/main/java/com/example/fn/HelloFunction.java
+++ invoke-other-functions/client/src/main/java/com/example/fn/HelloFunction.java
@@ -1,12 +1,18 @@
 package com.example.fn;
 
+import com.fnproject.fn.api.InvocationContext;
+
+import java.util.logging.Logger;
+
 public class HelloFunction {
+    private static final Logger LOGGER = Logger.getLogger(HelloFunction.class.getName());
 
-    public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
-
-        System.out.println("Inside Java Hello World function"); 
-        return "Hello, " + name + "!";
+    public String handleRequest(InvocationContext ctx) {
+        var fnOcid = ctx.getRuntimeContext().getConfiguration().get("FUNCTIONS_OCID");
+        ctx.getRuntimeContext().getConfiguration().forEach((k, v) -> {
+            LOGGER.info(String.format("%s: %S", k, v));
+        });
+        return fnOcid;
     }
 
 }
