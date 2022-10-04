f9011ac00a3114e4ba216e44de61cc8d94eb98e0
shukawam <shukawam@gmail.com>
2022-10-04 18:23:25 +0900

    WIP

--- apm-tracing/greeting/src/main/java/com/example/fn/HelloFunction.java
+++ apm-tracing/greeting/src/main/java/com/example/fn/HelloFunction.java
@@ -1,12 +1,24 @@
 package com.example.fn;
 
+import com.fnproject.fn.api.httpgateway.HTTPGatewayContext;
+
+import java.util.logging.Logger;
+
 public class HelloFunction {
+    private static final Logger LOGGER = Logger.getLogger(HelloFunction.class.getName());
 
-    public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
+    public String handleRequest(String input, HTTPGatewayContext ctx) {
+        String name = (input == null || input.isEmpty()) ? "world" : input;
 
-        System.out.println("Inside Java Hello World function"); 
+        System.out.println("Inside Java Hello World function");
+        loggingHeaders(ctx);
         return "Hello, " + name + "!";
+    }
+
+    private void loggingHeaders(HTTPGatewayContext ctx) {
+        ctx.getHeaders().asMap().forEach((k, v) -> {
+            LOGGER.info(String.format("%s: %s", k, v));
+        });
     }
 
 }
