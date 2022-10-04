9b4cccd8be69ac18868b60fc8766830b8cd634d8
shukawam <shukawam@gmail.com>
2022-10-04 10:31:30 +0900

    WIP

--- invoke-other-functions/client/src/main/java/com/example/fn/HelloFunction.java
+++ invoke-other-functions/client/src/main/java/com/example/fn/HelloFunction.java
@@ -1,18 +1,54 @@
 package com.example.fn;
 
 import com.fnproject.fn.api.InvocationContext;
+import com.fnproject.fn.api.httpgateway.HTTPGatewayContext;
+import com.oracle.bmc.auth.InstancePrincipalsAuthenticationDetailsProvider;
+import com.oracle.bmc.functions.FunctionsInvokeClient;
+import com.oracle.bmc.functions.requests.InvokeFunctionRequest;
+import com.oracle.bmc.functions.responses.InvokeFunctionResponse;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
 public class HelloFunction {
     private static final Logger LOGGER = Logger.getLogger(HelloFunction.class.getName());
 
-    public String handleRequest(InvocationContext ctx) {
-        var fnOcid = ctx.getRuntimeContext().getConfiguration().get("FUNCTIONS_OCID");
-        ctx.getRuntimeContext().getConfiguration().forEach((k, v) -> {
-            LOGGER.info(String.format("%s: %S", k, v));
-        });
-        return fnOcid;
+    public String handleRequest(HTTPGatewayContext ctx) {
+        var endpoint = ctx.getInvocationContext().getRuntimeContext().getConfiguration().get("FUNCTIONS_ENDPOINT");
+        var fnOcid = ctx.getInvocationContext().getRuntimeContext().getConfiguration().get("FUNCTIONS_OCID");
+        var name = ctx.getQueryParameters().get("name").orElse("Joe");
+        return invokeOtherFunctions(endpoint, fnOcid, name);
+    }
+
+    private String invokeOtherFunctions(String endpoint, String fnOcid, String name) {
+        InstancePrincipalsAuthenticationDetailsProvider provider = InstancePrincipalsAuthenticationDetailsProvider
+                .builder()
+                .build();
+        FunctionsInvokeClient invokeClient = FunctionsInvokeClient
+                .builder()
+                .endpoint(endpoint)
+                .build(provider);
+        InputStream inputStream = new ByteArrayInputStream(name.getBytes(StandardCharsets.UTF_8));
+        InvokeFunctionRequest request = InvokeFunctionRequest
+                .builder()
+                .functionId(fnOcid)
+                .fnInvokeType(InvokeFunctionRequest.FnInvokeType.Sync)
+                .fnIntent(InvokeFunctionRequest.FnIntent.Httprequest)
+                .body$(inputStream)
+                .build();
+        InvokeFunctionResponse response = invokeClient.invokeFunction(request);
+        BufferedReader br = new BufferedReader(new InputStreamReader(response.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        try {
+            String functionsResponse;
+            while ((functionsResponse = br.readLine()) != null) {
+                sb.append(functionsResponse);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 
 }
