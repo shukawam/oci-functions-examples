acd1af4ba9e2946240824f6788034d401e4e7388
shukawam <shukawam@gmail.com>
2023-05-30 00:50:55 +0900

    Fixed

--- oci-sch-streaming-json-to-csv/src/main/java/me/shukawam/EntryPoint.java
+++ oci-sch-streaming-json-to-csv/src/main/java/me/shukawam/EntryPoint.java
@@ -6,7 +6,6 @@ import java.util.logging.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.fnproject.fn.api.Headers;
 import com.fnproject.fn.api.OutputEvent;
 import com.fnproject.fn.api.OutputEvent.Status;
@@ -20,21 +19,19 @@ public class EntryPoint {
     private static final Logger logger = Logger.getLogger(EntryPoint.class.getName());
 
     public OutputEvent handleRequest(List<Input> inputs) {
-        CsvMapper csvMapper = new CsvMapper();
-        // logging input contents.
+        var csvMapper = new CsvMapper();
         inputs.forEach(input -> {
             logger.info(input.toString());
         });
-        CsvSchema csvSchema = csvMapper.schemaFor(Input.class).withHeader();
+        var csvSchema = csvMapper.schemaFor(Input.class).withHeader();
         try {
             var decodedInputs = inputs.stream().map(i -> new Input(i.getStream(), i.getPartition(),
                     base64Decode(i.getKey()), base64Decode(i.getValue()), i.getOffset(), i.getTimestamp())).toList();
             var csvOutput = csvMapper.writer(csvSchema).writeValueAsString(decodedInputs);
-            OutputEvent outputEvent = OutputEvent.fromBytes(
+            var outputEvent = OutputEvent.fromBytes(
                     csvOutput.getBytes(), Status.Success, "text/csv", Headers.emptyHeaders());
             return outputEvent;
         } catch (JsonProcessingException e) {
-            // some error processing.
             e.printStackTrace();
             throw new RuntimeException(e.getMessage());
         }
--- oci-sch-streaming-json-to-csv/src/test/java/me/shukawam/EntryPointTest.java
+++ oci-sch-streaming-json-to-csv/src/test/java/me/shukawam/EntryPointTest.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 import org.junit.Rule;
 import org.junit.Test;
 
-import com.fnproject.fn.testing.FnResult;
 import com.fnproject.fn.testing.FnTestingRule;
 
 public class EntryPointTest {
@@ -24,14 +23,16 @@ public class EntryPointTest {
                     {"stream": "mynewstream","partition": "0","key": null,"value": "eyJuYW1lIjoiYnJpYW4iLCJmaWVsZDEiOiJ0ZXN0In0=","offset": 0,"timestamp": "2018-04-26T01:03:06.051Z"}
                 ]
                         """;
+        logger.info(input);
         testing.givenEvent().withBody(input).enqueue();
         testing.thenRun(EntryPoint.class, "handleRequest");
-        FnResult result = testing.getOnlyResult();
+        var result = testing.getOnlyResult();
         var expectedOutput = """
-            key,offset,partition,stream,timestamp,value
-            ,0,0,mynewstream,1524704586051,"{""name"":""igor"", ""location"": ""Dublin""}"
-            ,0,0,mynewstream,1524704586051,"{""name"":""brian"",""field1"":""test""}"
-                """;
+                key,offset,partition,stream,timestamp,value
+                ,0,0,mynewstream,1524704586051,"{""name"":""igor"", ""location"": ""Dublin""}"
+                ,0,0,mynewstream,1524704586051,"{""name"":""brian"",""field1"":""test""}"
+                    """;
+        logger.info(result.getBodyAsString());
         assertEquals(expectedOutput, result.getBodyAsString());
     }
 }
