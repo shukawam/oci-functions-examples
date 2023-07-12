331b02e38be849f51ed6f98272d45c9a42f0854d
shukawam <shukawam@gmail.com>
2023-07-12 14:51:40 +0900

    Fixed

--- object-storage-client/func.go
+++ object-storage-client/func.go
@@ -3,14 +3,14 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 
 	fdk "github.com/fnproject/fdk-go"
 
-	"github.com/oracle/oci-go-sdk/v49/common"
-	"github.com/oracle/oci-go-sdk/v49/common/auth"
-	"github.com/oracle/oci-go-sdk/v49/example/helpers"
-	"github.com/oracle/oci-go-sdk/v49/objectstorage"
+	"github.com/oracle/oci-go-sdk/v65/common"
+	"github.com/oracle/oci-go-sdk/v65/common/auth"
+	"github.com/oracle/oci-go-sdk/v65/objectstorage"
 )
 
 func main() {
@@ -30,12 +30,15 @@ func listBucket(ctx context.Context, client objectstorage.ObjectStorageClient, n
 }
 
 func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
-	provider, err := auth.ResourcePrincipalConfigurationProvider()
-	helpers.FatalIfError(err)
+	rp, err := auth.InstancePrincipalConfigurationProvider()
+	if err != nil {
+		panic(err)
+	}
+	objectStorageClient, err := objectstorage.NewObjectStorageClientWithConfigurationProvider(rp)
+	if err != nil {
+		fmt.Printf("failed to create ObjectStorageClient : %s", err)
+	}
 
-	client, client_err := objectstorage.NewObjectStorageClientWithConfigurationProvider(provider)
-	helpers.FatalIfError(client_err)
-
-	items := listBucket(ctx, client, "orasejapan", "ocid1.compartment.oc1..aaaaaaaanjtbllhqxcg67dq7em3vto2mvsbc6pbgk4pw6cx37afzk3tngmoa")
+	items := listBucket(ctx, objectStorageClient, "orasejapan", "ocid1.compartment.oc1..aaaaaaaanjtbllhqxcg67dq7em3vto2mvsbc6pbgk4pw6cx37afzk3tngmoa")
 	json.NewEncoder(out).Encode(&items)
 }
--- object-storage-client/go.mod
+++ object-storage-client/go.mod
@@ -2,9 +2,13 @@ module func
 
 go 1.19
 
-require github.com/fnproject/fdk-go v0.0.33
+require (
+	github.com/fnproject/fdk-go v0.0.33
+	github.com/oracle/oci-go-sdk/v65 v65.43.0
+)
 
 require (
-	github.com/oracle/oci-go-sdk/v49 v49.2.0 // indirect
-	github.com/sony/gobreaker v0.4.2-0.20210216022020-dd874f9dd33b // indirect
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/sony/gobreaker v0.5.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 )
