6c35928cfd36dad5c12b6887f7da9537a0cff564
shukawam <shukawam@gmail.com>
2023-10-12 23:30:50 +0900

    Fixed test code

--- greeting-java/src/test/java/me/shukawam/fn/EntryPointTest.java
+++ greeting-java/src/test/java/me/shukawam/fn/EntryPointTest.java
@@ -18,15 +18,24 @@ public class EntryPointTest {
         testing.thenRun(EntryPoint.class, "handleRequest");
 
         FnResult result = testing.getOnlyResult();
-        assertEquals("{\"message\":\"Hello world\"}", result.getBodyAsString());
+        assertEquals("{\"message\":\"Hi there.\"}", result.getBodyAsString());
     }
 
-    @Test
-    public void shouldReturnCustomMessage() {
-        testing.givenEvent().withBody("John").enqueue();
-        testing.thenRun(EntryPoint.class, "handleRequest");
+    // @Test
+    // public void shouldReturnDefaultMessage() {
+    //     testing.givenEvent().enqueue();
+    //     testing.thenRun(EntryPoint.class, "handleRequest");
 
-        FnResult result = testing.getOnlyResult();
-        assertEquals("{\"message\":\"Hello John\"}", result.getBodyAsString());
-    }
+    //     FnResult result = testing.getOnlyResult();
+    //     assertEquals("{\"message\":\"Hello world\"}", result.getBodyAsString());
+    // }
+
+    // @Test
+    // public void shouldReturnCustomMessage() {
+    //     testing.givenEvent().withBody("John").enqueue();
+    //     testing.thenRun(EntryPoint.class, "handleRequest");
+
+    //     FnResult result = testing.getOnlyResult();
+    //     assertEquals("{\"message\":\"Hello John\"}", result.getBodyAsString());
+    // }
 }
